5a6a7a9783f5466f8317e3e82ba993bac81946e4
VONTHRON177013 <60663405+VONTHRON177013@users.noreply.github.com>
2020-02-11 12:54:38 +0100

    Create coup_possible.py

---
+++ coup_possible.py
@@ -0,0 +1,21 @@
+def coup_possible(gril,col):
+#Détermine si il est possible de jouer dans la colonne col
+#Prend en argument la grille, tableau 5x6 avec la position des pions des joeueurs et un entier (numéro de la colonne entre 0 et 6)
+#Renvoie True si possible de jouer, False sinon
+    col1=[i[col] for i in gril]
+    if 0 in col1:
+        return(True)
+    else:
+        return(False)
+assert coup_possible([[0, 0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 0, 0, 1]],6)==False
+assert coup_possible([[0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0]],5)==True
