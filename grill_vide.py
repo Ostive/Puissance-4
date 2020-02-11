b726d86458a92da9948c5e6bb66cf3830e6d7966
VONTHRON177013 <60663405+VONTHRON177013@users.noreply.github.com>
2020-02-11 12:36:25 +0100

    Update grill_vide.py
    Ajouté une assertion.

--- grill_vide.py
+++ grill_vide.py
@@ -1,8 +1,15 @@
 def grille_vide():
     #fonction construit un tableau 6*7
     #6 ligne 7 colonne
-   #chaque case ==0
+    #chaque case ==0
     #prend pas d argument
     #renvoie le tableau
     a=[[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0]]
     return(a)
+assert grille_vide()==  [[0, 0, 0, 0, 0, 0, 0],
+                        	   [0, 0, 0, 0, 0, 0, 0],
+                        	   [0, 0, 0, 0, 0, 0, 0],
+                        	   [0, 0, 0, 0, 0, 0, 0],
+                        	   [0, 0, 0, 0, 0, 0, 0],
+                        	   [0, 0, 0, 0, 0, 0, 0]]
+
