885dec24ee607d9a70bd572476f9d49b08a728be
PrinceAiba <60670455+PrinceAiba@users.noreply.github.com>
2020-02-11 14:08:23 +0200

    match_nul.py

---
+++ match_nul.py
@@ -0,0 +1,8 @@
+def match_nul(gril):
+    	if 0 in gril[0]:
+        		return(False)
+   	 else:
+                       if victoire(gril,j1)==false and victoire(gril,j2)==false:
+        		return(True)
+assert match_nul(gril)==False
+assert match_nul(gril)==True
